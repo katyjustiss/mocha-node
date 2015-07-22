8f47ed03c1e056879ed60516d9f5872fc7665f6d
Katy Justiss <katy.justiss@gmail.com>
2015-07-22 09:41:38 -0500

    added Cheers tests

--- test/test.js
+++ test/test.js
@@ -22,20 +22,20 @@ describe('CLI', function() {
 });
 
 //Tests for the cheers function
-// describe.only('Cheers', function() {
-//   it('should create a cheer from name', function(done) {
-//     cp.execFile('./lib/Cheers.js', ['Katy'], function(err, stdout) {
-//       stdout.should.equal("Give me a K!\nGive me an A!\nGive me a T!\nGive me a Y!\n");
-//       done();
-//     })
-//   })
-//   it('should create cheer that can handle spaces', function(done) {
-//     cp.execFile('./lib/Cheers.js', ['Katy', 'Ann'], function(err, stdout) {
-//       stdout.should.equal("Give me a K!\nGive me an A!\nGive me a T!\nGive me a Y!\nGive me an A!\nGive me an N!\nGive me an N!\n");
-//       done();
-//     })
-//   })
-// });
+describe.only('Cheers', function() {
+  it('should create a cheer from name', function(done) {
+    cp.execFile('./lib/Cheers.js', ['Katy'], function(err, stdout) {
+      stdout.should.equal("Give me a K!\nGive me an A!\nGive me a T!\nGive me a Y!\n");
+      done();
+    })
+  })
+  it('should create cheer that can handle spaces', function(done) {
+    cp.execFile('./lib/Cheers.js', ['Katy', 'Ann'], function(err, stdout) {
+      stdout.should.equal("Give me a K!\nGive me an A!\nGive me a T!\nGive me a Y!\nGive me an A!\nGive me an N!\nGive me an N!\n");
+      done();
+    })
+  })
+});
 
 
 describe('Animal', function() {
